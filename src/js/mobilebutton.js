ec649678b157bf2059494be41ba370d5cb20aac0
nicolaswalcker <walcker_@outlook.com>
2021-11-30 19:18:48 -0300

    style: adjust planet image and info contents

--- src/js/mobilebutton.js
+++ src/js/mobilebutton.js
@@ -1,7 +1,11 @@
 const mobileButton = document.querySelector('.js-mobile');
 const navbar = document.querySelector('.js-navbar');
+const body = document.querySelector('body');
 
 mobileButton.addEventListener('click', ()=>{
     mobileButton.classList.toggle('u-button--active');
     navbar.classList.toggle('u-navbar--active');
+    if(navbar.classList.contains("u-navbar--active")){
+      body.style.overflow = "hidden";
+    }
 })
